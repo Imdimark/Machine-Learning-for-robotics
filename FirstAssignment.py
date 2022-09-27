b40455d1dd037d084857ebb6500b646aa520cb82
Imdimark <giovannidimarco06@gmail.com>
2022-09-27 20:48:04 +0200

    some improvements

---
+++ FirstAssignment.py
@@ -0,0 +1,21 @@
+import ast
+from importlib.resources import contents
+import numpy
+import pandas as pd
+
+ciao = pd.read_table('Weatherdataset')
+print (ciao)
+
+
+
+
+#file = open("Weatherdataset", "r")
+#dictionary = {file.read()}
+#print(dictionary)
+#dataframe=pd.DataFrame(dictionary)
+
+
+
+
+print (dataframe)
+

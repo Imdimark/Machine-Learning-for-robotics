b6651c7842dd7cd16245fa05184b13dab5022be8
Imdimark <78663960+Imdimark@users.noreply.github.com>
2022-10-10 23:53:29 +0200

    Add files via upload

--- FirstAssignment.py
+++ FirstAssignment.py
@@ -6,8 +6,6 @@ import numpy as np
 import pandas as pd
 import csv
 import os
-
-
 if os.path.exists("Weatherdataset.csv"):
   os.remove("Weatherdataset.csv")
 read_file= pd.read_csv('Weatherdataset.txt', sep=" ", usecols= ['Outlook','Temperature','Humidity','Windy','Play'])
@@ -16,76 +14,58 @@ wd = pd.read_csv('Weatherdataset.csv')
 wd.head()
 number_attributes = (wd.shape[1] - 1 )#shape [righe][colonne]
 number_set = wd.shape[0]
-#print(wd.head())
-
-attributes = {"Outlook": {'overcast', 'rainy', 'sunny'}, "Temperature":{'hot', 'cool', 'mild'}, "Humidity":{'high', 'normal'}, "Windy":{'FALSE', 'TRUE'}}
+print("\nSome line of dataset are: ")
+print(wd.head())
+print ("\n")
+attributes = {"Outlook": {'overcast':None, 'rainy':None, 'sunny':None}, "Temperature":{'hot':None, 'cool':None, 'mild':None}, "Humidity":{'high':None, 'normal':None}, "Windy":{'FALSE':None, 'TRUE':None}}
 listed_attributes=  list (attributes) #outlook temperature humidity
+##########################################
+print("\nStarting learning:") 
+px = wd.mean(axis = 0)
+PxT = px[len(px) -1 ] #prior probability of classe true
+PxF = 1- PxT #prior probability of classe false)
 for attribute in range (number_attributes):
   listed_attribute = list(attributes [listed_attributes[attribute]])
-
   for attribute_member in range(len(listed_attribute)):
-    #print(listed_attribute[attribute_member])
-    #print( (wd.query('Play')[listed_attributes[attribute]]== listed_attribute[attribute_member]).mean() )
-    print(listed_attributes[attribute] + "confrontato con" + listed_attribute[attribute_member])
-    pippo = (wd.query('Play')[listed_attributes[attribute]]== listed_attribute[attribute_member]).mean()
-    #attributes[listed_attributes[attribute]][listed_attribute[attribute_member]] = pippo
+    if ((listed_attribute[attribute_member] == "TRUE") or (listed_attribute[attribute_member] == "FALSE" )): #is a boolean
+      #pr = (wd.query('Play')[listed_attributes[attribute]]).mean() #pr1 is P(X|y)
+      pr2 =(wd.query (f'{listed_attributes[attribute]}')['Play']).mean() #skipna = true (default)
+      # pr2 is P(y|X)  X={overcast, rainy, sunny} escono 3 di queste probabilità e le managio
+      #So basically, P(y|X) here means, the probability of “Not playing golf” given that the weather conditions are “Rainy outlook”, “Temperature is hot”, “high humidity” and “no wind”.
+    else: #is not boolean
+      pr2 =(wd.query (f'{listed_attributes[attribute]} == "{listed_attribute[attribute_member]}"')['Play']).mean()
+      #pr = (wd.query('Play')[listed_attributes[attribute]] == listed_attribute[attribute_member]).mean() 
+      #print(f'{listed_attributes[attribute]} == {listed_attribute[attribute_member]}')
+    attributes[listed_attributes[attribute]][listed_attribute[attribute_member]] = pr2 #pr2 = (pr * py)/px   #p(X|y)P(y)/P(x)
+print ("\nDict of probabilities: ")
+print (attributes)
+print ("\n")
+##########################################
+print ("\nStarting input and comparing:\n")
 list_in_input=[]
-probability = 0
+array_for_prod = []
+flag = 0
 while (1):
   for insert in range (number_attributes):
     digited = input("insert attribute, q for quit" + listed_attributes[insert])
-    list_in_input.append(digited)
-    probability += attributes
-    print (probability)
+    list_in_input.append(digited) 
+    PXiY = attributes[listed_attributes[attribute]][listed_attribute[attribute_member]]
+    array_for_prod.append(PXiY)
     if (digited == "q"):
-      exit
+      break
+
+  print ("You have inserted these attribute: ")
   print (list_in_input)
-  print (probability)
-  if (input == "q"):
-    exit
-
-
-
-"""print( (wd.query('Play')[Attributes[0]]=='overcast').mean() )
-print( (wd.query('Play')[Attributes[0]]=='rainy').mean() )
-print( (wd.query('Play')[Attributes[0]]=='sunny').mean() )"""
-
-#print( (wd.query('Play')[attributes[0]]=='rainy').mean() )
-#print( (wd.query('Play')['Outlook']=='rainy').mean() )
-
-
-
-
-
-#Weatherdataset = open('Weatherdataset.csv', mode='r')
-#csv_filename = 'Weatherdataset.csv'
-#with open('Weatherdataset.csv', newline='') as f:
-#    reader = csv.reader(f)
-#    data = list(reader)
-#number_set = len(data)
-#attributes = 4
-#Outlook = ["overcast", "rainy", "sunny"]  
-#Temperature = ["hot", "cool", "mild"]
-#Temperature = ["hot", "cool", "mild"]
-#for y in range (0, 1, attributes):
-  #for x in range(1, 1, number_set):
-    #riga colonna
-    #if data [x][y] ==  
-
-
-"""for convert in number_set:
-  if (wd.head([convert][number_attributes]) == "yes"):
-    wd.head([convert][number_attributes]) = True
+  print ("\n")
   
-  if else:
-    wd.head([convert][number_attributes]) = False"""
+  gx= PxT * np.prod(array_for_prod)
+  print(gx)
+  array_for_prod = [] #reset of the array
+  list_in_input = [] #reset of the array
+  digited = input("insert attribute, q (second time) for quit")
   
-"""Outlook = {'overcast', 'rainy', 'sunny'}
-Temperature = {'hot', 'cool', 'mild'}
-Humidity = {'high', 'normal'}
-Windy = {'FALSE', 'TRUE'}
-attributes = {"Outlook": Outlook, "Temperature":Temperature, "Humidity":Humidity, "Windy":Windy}"""
-
-
+  if (digited == "q"):
+    break
+  flag = 0
 
 

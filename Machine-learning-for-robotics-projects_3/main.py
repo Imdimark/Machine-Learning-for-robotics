4d22a32bdb9589ed85f5eec713b6101fdec8ec38
Imdimark <78663960+Imdimark@users.noreply.github.com>
2022-11-06 20:00:09 +0100

    Add files via upload

--- Machine-learning-for-robotics-projects_3/main.py
+++ Machine-learning-for-robotics-projects_3/main.py
@@ -12,71 +12,134 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-
-(train_X, train_y), (test_X, test_y) = mnist.load_data()
-
-train_X = tf.keras.utils.normalize(train_X, axis = 1)
-test_X = tf.keras.utils.normalize(test_X, axis = 1)
+import sys
+import datetime
 
 def calculate_dist(x,y): 
-    
-    #dist = np.sqrt(np.sum(np.power(2,np.subtract(y,x))))
-    dist = np.sum((y - x)**2)**.5
+    #sum = 0
+    #for X in range (len(x)):
+    #    for Y in range (len(y)):
+    #        sum = sum + np.square(x[X][Y] - y[X][Y])  #too slow
+    #return (np.sqrt(sum))
+    #return (np.linalg.norm(x - y, ord=2))
+    #return np.sqrt(np.sum((x - y) ** 2))   
+    x = x.flatten() #convert in 1D
+    y = y.flatten()
+    dist = np.sum((y - x)**2)**.5 #euclidean distance
     return (dist)
 
+def Classifier (test_X_images, test_y_labels, train_X_images, train_y_labels, k, classes): #if the number of args is < 5 then the execution will be interrupted
+    cont = 0
+    dist = []
+    winner_array =[]
+    #numbers_of_neig = []
+    counter_ = [0,0,0,0,0,0,0,0,0,0] #memorizes the occourrences of single classe
+    for ttest in range (len(test_X_images)):       
+        for test in range (len (train_X_images)):
+            dist.append(calculate_dist(test_X_images[ttest],train_X_images[test]))
+        dist_sorted = np.sort(dist)
+        for range_k in range (k):      
+            index = dist.index(dist_sorted[range_k])
+            for cc in range (len(classes)):
+                if train_y_labels[index] == classes[cc]:
+                    counter_[cc] = counter_[cc] + 1       
+        winner = np.argmax(counter_)  # in case of two or more max with the same values takes the first encountered
+        winner_array.append(winner) #this memorize the classification for the courrent k
+        if (winner == test_y_labels[ttest]):
+            cont = cont + 1 #to optimize the space i prefere to mantain an integer instead a one more column (1x10000)        
+        dist = [] #clean the memory
+        counter_ = [0,0,0,0,0,0,0,0,0,0]
+    denominatore = len(test_X_images)
+    correct_rate= (cont/denominatore)*100 
+    return (winner_array, correct_rate)
+
+############# init #############
+start = datetime.datetime.now()
+print ("starting time: " + str (start))
+(train_X, train_y), (test_X, test_y) = mnist.load_data()
+train_X = tf.keras.utils.normalize(train_X, axis = 1) #optimization
+test_X = tf.keras.utils.normalize(test_X, axis = 1)
 number_of = 60000
+#nargin_min = 5
 number_kTest = [1,2,3,4,5,10,15,20,30,40,50]
+#number_kTest = [1,2,3]
 classes = [0,1,2,3,4,5,6,7,8,9]
-correct_rate = []
-test_X_images = np.asarray(test_X)
+correct_rate = [] #memorizes the various correct rate 
+classification_array = []
+test_X_images = np.asarray(test_X) 
 test_y_labels = np.asarray(test_y)
 train_X_images = np.asarray(train_X[:number_of])
 train_y_labels = np.asarray(train_y[:number_of])
-###########check n columns############
-for k_ in range (len(number_kTest)):
-    k = number_kTest[k_]
-    cont = 0
-    dist = []
-    k_min = []
-    #numbers_of_neig = []
-    counter_ = [0,0,0,0,0,0,0,0,0,0]
-    for ttest in range (len(test_X_images)):
-        
-        for test in range (len (train_X_images)):
 
-            dist.append(calculate_dist(test_X_images[ttest],train_X_images[test]))
+############# checks #############
+if np.max(number_kTest) >= number_of and np.max(number_kTest) > 0: #check  0 < k <= number of images used from training set
+    print ("error in k number")
+    sys.exit()
 
+if len(test_X_images[1])!= len(train_X_images[1]): #check taht training set e test set has the same column
+    print ("error in number of columns")
+    sys.exit()
+############# point 2 #############
+correct_rate_array = []
+for k_ in range (len(number_kTest)):    
+    k = number_kTest[k_] #k that i'm using in this cycle
+    print ("Calculation under this K: " + str(k))
+    winner_array,correct_rate= Classifier (test_X_images, test_y_labels, train_X_images, train_y_labels, k, classes)
+    classification_array.append(winner_array)
+    correct_rate_array.append (correct_rate)
+duration = datetime.datetime.now() - start
+print ("duration of the point 2:" + str(duration))
+#stamp = input ('Do you want to print the array with the test set classification for all the K? Y/N')
+print ("Next step: a graph with the errors rate will be shown")
+#if (stamp == "Y"):
+#    print (winner_array)
+fig, ax = plt.subplots(figsize=(8,6))
+ax.plot(number_kTest, correct_rate_array)
+ax.set_xlabel('number of Nearest Neighbors (k)')
+ax.set_ylabel('Accuracy (%)')
+ax.set_title("error rate depending on k")
+plt.show()
 
-        dist_sorted = np.sort(dist)
+############# point 3 #############
+correct_rate = [] #clean the array
+correct_rate_array = []
+old_classes = classes
+classes = [0,1] #now they are 2, 0 and 1, 1 if it is the digit
+start = datetime.datetime.now()
+print ("starting time 3 point: " + str (start))
+fig, ax = plt.subplots(10,1)
 
-        for range_k in range (k):      
-            index = dist.index(dist_sorted[k])
-            #numbers_of_neig.append(train_y_labels[index])
+for t in range (len(old_classes)):
+    print ("Number: " + str(t) ) 
+    test_y_labels_2classes = []
+    train_y_labels_2classes = []
+    number_1 = 0
+    print(test_y_labels)
+    for i in range (len (test_X_images)):
+        if test_y_labels[i] == t:
+            test_y_labels_2classes.append(1) 
+            number_1 += 1  
+        else:
+            test_y_labels_2classes.append(0)
+    #print(test_y_labels,test_y_labels_2classes)
+    for i in range (len (train_X_images)):    
+        if train_y_labels [i] == t:
+            train_y_labels_2classes.append(1)  
+        else:
+            train_y_labels_2classes.append(0)
+    #print(train_y_labels,train_y_labels_2classes)
+    for k_ in range (len(number_kTest)):    
+        k = number_kTest[k_] #k that i'm using in this cycle
+        print ("Calculation under this K: " + str(k))
+        winner_array,correct_rate = Classifier (test_X_images, test_y_labels_2classes, train_X_images, train_y_labels_2classes, k, classes)
+        correct_rate_array.append(correct_rate)
+    ax[t].plot(number_kTest, correct_rate_array)
+    ax[t].set_title ('Error rate depending on various K, of the number: '+ str(t))
+    ax[t].set_xlabel('number of Nearest Neighbors (k)')
+    ax[t].set_ylabel('Accuracy (%)')
+    correct_rate_array = []
+duration = datetime.datetime.now() - start
+print ("duration of the point 3 :" + str(duration))
+plt.show()
 
-            for cc in range (len(classes)):
-                if train_y_labels[index] == classes[cc]:
-                    counter_[cc] = counter_[cc] + 1
-
-        winner = np.argmax(counter_)     
-        #print (counter_, winner)      
-        '''most_common = max(k_min, key = k_min.count)
-        
-        print (k_min,most_common)
-
-        inde = list.index(dist,most_common)
-        #index = np.ndarray.argmin(np.array(most_common))
-        
-        nume = train_y_labels[inde]
-        
-        plt.imshow(test_X_images[ttest])
-        plt.show()'''
-        nume = winner
-        if (nume == test_y_labels[ttest]):
-            cont = cont + 1
-        dist = []
-        k_min = []
-        #numbers_of_neig = []
-        counter_ = [0,0,0,0,0,0,0,0,0,0]
-    correct_rate.append((cont/len(test_X_images))*100)
-
-print (correct_rate)
+      

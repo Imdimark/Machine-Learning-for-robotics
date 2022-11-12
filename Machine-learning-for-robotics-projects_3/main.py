64c5a5d24b85dfe10b30c85952caba1d9c8f51cc
Imdimark <78663960+Imdimark@users.noreply.github.com>
2022-11-12 17:52:41 +0100

    Update main.py
    Adding additional point with confusion matrix

--- Machine-learning-for-robotics-projects_3/main.py
+++ Machine-learning-for-robotics-projects_3/main.py
@@ -15,6 +15,28 @@ import numpy as np
 import sys
 import datetime
 
+def additional_point(confusion_matrix):
+    array = np.zeros(6)
+    n_correct = np.trace(confusion_matrix)    
+    accuracy = n_correct/len(test_X_images) # is equal to correct rate calculated in point 3
+
+    correct_0 = confusion_matrix[0][0]   # 1 if it is the number, 0 if not
+    false_0 = confusion_matrix[0][1]
+    correct_1 = confusion_matrix[1][0]   
+    false_1 = confusion_matrix[1][1]
+    sensitivity = correct_1/(correct_1 + false_0)     #probability to correctly recognize class 1
+    specifiticy = correct_0/(correct_0 + false_1)
+    precision = correct_1/(correct_1 + false_1)
+    recall = sensitivity
+    F_measure = 2 * ( (precision *recall)/(precision + recall) )
+    array[0] = (sensitivity)
+    array[1] = (specifiticy)
+    array[2] = (precision)
+    array[3] = (recall)
+    array[4] = (F_measure)
+    array[5] = (accuracy)   
+    return array
+
 def calculate_dist(x,y): 
     #sum = 0
     #for X in range (len(x)):
@@ -32,6 +54,7 @@ def Classifier (test_X_images, test_y_labels, train_X_images, train_y_labels, k,
     cont = 0
     dist = []
     winner_array =[]
+    confusion_matrix = np.zeros((len(classes), len(classes)))
     #numbers_of_neig = []
     counter_ = [0,0,0,0,0,0,0,0,0,0] #memorizes the occourrences of single classe
     for ttest in range (len(test_X_images)):       
@@ -44,22 +67,22 @@ def Classifier (test_X_images, test_y_labels, train_X_images, train_y_labels, k,
                 if train_y_labels[index] == classes[cc]:
                     counter_[cc] = counter_[cc] + 1       
         winner = np.argmax(counter_)  # in case of two or more max with the same values takes the first encountered
-        winner_array.append(winner) #this memorize the classification for the courrent k
+        winner_array.append(winner) #this memorize the classification for the courrent k 
+        confusion_matrix[test_y_labels[ttest]] [winner] += 1    
         if (winner == test_y_labels[ttest]):
             cont = cont + 1 #to optimize the space i prefere to mantain an integer instead a one more column (1x10000)        
         dist = [] #clean the memory
         counter_ = [0,0,0,0,0,0,0,0,0,0]
     denominatore = len(test_X_images)
     correct_rate= (cont/denominatore)*100 
-    return (winner_array, correct_rate)
-
+    return (winner_array, correct_rate, confusion_matrix)
 ############# init #############
 start = datetime.datetime.now()
 print ("starting time: " + str (start))
 (train_X, train_y), (test_X, test_y) = mnist.load_data()
 train_X = tf.keras.utils.normalize(train_X, axis = 1) #optimization
 test_X = tf.keras.utils.normalize(test_X, axis = 1)
-number_of = 60000
+number_of = 6000
 #nargin_min = 5
 number_kTest = [1,2,3,4,5,10,15,20,30,40,50]
 #number_kTest = [1,2,3]
@@ -75,7 +98,6 @@ train_y_labels = np.asarray(train_y[:number_of])
 if np.max(number_kTest) >= number_of and np.max(number_kTest) > 0: #check  0 < k <= number of images used from training set
     print ("error in k number")
     sys.exit()
-
 if len(test_X_images[1])!= len(train_X_images[1]): #check taht training set e test set has the same column
     print ("error in number of columns")
     sys.exit()
@@ -115,24 +137,26 @@ for t in range (len(old_classes)):
     train_y_labels_2classes = []
     number_1 = 0
     print(test_y_labels)
-    for i in range (len (test_X_images)):
+    
+    for i in range (len (test_X_images)):#here converts the number taken in consideration as 1 and the othe 9 as 0
         if test_y_labels[i] == t:
             test_y_labels_2classes.append(1) 
             number_1 += 1  
         else:
             test_y_labels_2classes.append(0)
-    #print(test_y_labels,test_y_labels_2classes)
     for i in range (len (train_X_images)):    
         if train_y_labels [i] == t:
             train_y_labels_2classes.append(1)  
         else:
             train_y_labels_2classes.append(0)
-    #print(train_y_labels,train_y_labels_2classes)
+
     for k_ in range (len(number_kTest)):    
         k = number_kTest[k_] #k that i'm using in this cycle
         print ("Calculation under this K: " + str(k))
         winner_array,correct_rate = Classifier (test_X_images, test_y_labels_2classes, train_X_images, train_y_labels_2classes, k, classes)
         correct_rate_array.append(correct_rate)
+
+        
     ax[t].plot(number_kTest, correct_rate_array)
     ax[t].set_title ('Error rate depending on various K, of the number: '+ str(t))
     ax[t].set_xlabel('number of Nearest Neighbors (k)')
@@ -142,4 +166,56 @@ duration = datetime.datetime.now() - start
 print ("duration of the point 3 :" + str(duration))
 plt.show()
 
-      
+############# additional point #############
+start = datetime.datetime.now()
+print ("starting time additional point: " + str (start))
+correct_rate = [] #clean the array
+correct_rate_array = []
+classes = [0,1] #now they are 2, 0 and 1, 1 if it is the digit
+table = [0 for x in range(len(number_kTest)* len(old_classes))]
+c = 0
+#table = [[0 for i in range(10)] for j in range(10)]
+#table = np.array((10,10))
+for t in range (len(old_classes)):
+    print ("Number: " + str(t) ) 
+    test_y_labels_2classes = []
+    train_y_labels_2classes = []
+    number_1 = 0
+
+    for i in range (len (test_X_images)): #here converts the number taken in consideration as 1 and the othe 9 as 0
+        if test_y_labels[i] == t:
+            test_y_labels_2classes.append(1) 
+            number_1 += 1  
+        else:
+            test_y_labels_2classes.append(0)
+    for i in range (len (train_X_images)):    
+        if train_y_labels [i] == t:
+            train_y_labels_2classes.append(1)  
+        else:
+            train_y_labels_2classes.append(0)
+
+    for k_ in range (len(number_kTest)):
+        k = number_kTest[k_] #k that i'm using in this cycle
+        print ("Calculation under this K: " + str(k))
+        pluto = np.zeros((10,6))
+        for sample in range(10):
+            indexs = np.random.randint (1, 10000, 1000)
+            test_X_images_sampled = test_X_images [indexs]
+            test_y_labels_sampled = test_y_labels [indexs]
+            winner_array,correct_rate,confusion_matrix = Classifier (test_X_images_sampled, test_y_labels_2classes, train_X_images, train_y_labels_2classes, k, classes)
+            pluto[sample] = additional_point(confusion_matrix) 
+        standard_deviation = np.zeros(6)
+        averages = [0,0,0,0,0,0,0,0]
+        for x in range (6):
+            standard_deviation[x] = np.std(pluto[:,x]) 
+            averages[0] = t
+            averages [1] = k             
+            averages[x+2] = ( str(np.mean(pluto[:,x])) + ('(') + str(np.std(pluto[:,x]) )  + (')') )  # sensitivity specificity precision recall f_measure recall
+        table[c ] = averages   # row = k, column = numbers
+        c = c + 1
+        
+df = pd.DataFrame(table, columns = ("number", "k_number" ,"sensitivity", "specificity", "precision", "recall", "f_measure", "recall"))
+df.to_csv("./Table.csv", sep=',',index=False)
+print (df)
+duration = datetime.datetime.now() - start
+print ("duration of the point 3 :" + str(duration))
